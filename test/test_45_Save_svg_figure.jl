6e95f8854d3b6cce1e4e4b640d521ee1910777d0
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2021-11-23 15:49:27 +0100

    test_45_Save_svg_figure.jl: Old print call corrected

--- test/test_45_Save_svg_figure.jl
+++ test/test_45_Save_svg_figure.jl
@@ -8,8 +8,7 @@ println("\n... test test_45_Save_svg_figure.jl:\n")
 
 t = range(0.0, stop=10.0, length=100)
 result = Dict{String,Any}("time" => t, "phi" => sin.(t))
-info   = resultInfo(result)
-showResultInfo(result)
+ModiaResult.printResultInfo(result)
 
 plot(result, "phi", figure=2)
 saveFigure(2, "test_saveFigure.svg")
